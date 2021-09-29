1b08826740748b568903e254d748c85f346d43a1
Md Firoj Kabir <firoj.kabir65@gmail.com>
2021-09-29 16:24:36 +0200

    Spread operators

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-//OBJECT DESTRUCTURING
+// OBJECT DESTRUCTURING
 const address = {
     street: '',
     city: '',
     country: '',
 };
 
-// previous style
+// old way
 const street = address.street;
 const city = address.city;
 const country = address.country;
 
-// ES6 style
-const { street, city, country } = address;
+// ES6 way
+// const { street, city, country } = address;
 const { street: st } = address; // change the 'street' property by 'st' using allias 
+
+// SPREAD OPERATOR
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+// old way
+// const combined = first.concat(second);
+// ES6 way
+const combined = [...first, 'a', ...second, 'b']; // possible to insert new elements
+
+const clone = [...first];
+console.log(first);
+console.log(combined);
