b803d7f13d9e7d0d86cca9af87937cce5e9e45e1
Md Firoj Kabir <firoj.kabir65@gmail.com>
2021-09-29 16:13:35 +0200

    Object destructuring

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //OBJECT DESTRUCTURING
+const address = {
+    street: '',
+    city: '',
+    country: '',
+};
+
+// previous style
+const street = address.street;
+const city = address.city;
+const country = address.country;
+
+// ES6 style
+const { street, city, country } = address;
+const { street: st } = address; // change the 'street' property by 'st' using allias 
